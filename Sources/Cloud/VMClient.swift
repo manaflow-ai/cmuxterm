c8328762c9261b8e5823d7cb4aaefb79c65a2ca8
Austin Wang <austinwang115@gmail.com>
2026-09-06 04:00:46 -0700

    fix: use compatible VM retry sleep

--- Sources/Cloud/VMClient.swift
+++ Sources/Cloud/VMClient.swift
@@ -2101,7 +2101,7 @@ actor VMClient {
                let delaySeconds = Self.transientVMRetryDelay(http: http, data: data) {
                 retriesLeft -= 1
                 onRetry()
-                try await Task.sleep(for: .seconds(delaySeconds))
+                try await Task.sleep(nanoseconds: UInt64(delaySeconds * 1_000_000_000))
                 continue
             }
             if let sessionIdentity {
