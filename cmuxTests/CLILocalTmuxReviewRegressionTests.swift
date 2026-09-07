6db8e7e4f4860429c3a75686413da5ceff098f44
Austin Wang <austinwang115@gmail.com>
2026-09-06 18:07:13 -0700

    test: import app module for local tmux regressions

--- cmuxTests/CLILocalTmuxReviewRegressionTests.swift
+++ cmuxTests/CLILocalTmuxReviewRegressionTests.swift
@@ -1,5 +1,10 @@
 import Darwin
 import XCTest
+#if canImport(cmux_DEV)
+@testable import cmux_DEV
+#elseif canImport(cmux)
+@testable import cmux
+#endif
 
 extension CLINotifyProcessIntegrationRegressionTests {
     func testLocalTmuxLifecycleRejectsAttachmentOptions() throws {
