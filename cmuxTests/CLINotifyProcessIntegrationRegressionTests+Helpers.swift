6075264dfc872372e48726a1ba66ae7cdb8846be
austinpower1258 <austinwang115@gmail.com>
2026-09-08 12:35:16 -0700

    fix: preserve test target import guards

--- cmuxTests/CLINotifyProcessIntegrationRegressionTests+Helpers.swift
+++ cmuxTests/CLINotifyProcessIntegrationRegressionTests+Helpers.swift
@@ -1,6 +1,10 @@
 import XCTest
 import Darwin
 #if canImport(cmux_DEV)
+@testable import cmux_DEV
+#elseif canImport(cmux)
+@testable import cmux
+#endif
 
 extension CLINotifyProcessIntegrationRegressionTests {
     func assertSSHPTYAttachAuthUsesRetryLoop(
--- cmuxTests/PiFeedDockOwnershipTests+Helpers.swift
+++ cmuxTests/PiFeedDockOwnershipTests+Helpers.swift
@@ -6,6 +6,9 @@ import Testing
 
 #if canImport(cmux_DEV)
 @testable import cmux_DEV
+#elseif canImport(cmux)
+@testable import cmux
+#endif
 
 extension PiFeedDockOwnershipTests {
     func withAppContext(
