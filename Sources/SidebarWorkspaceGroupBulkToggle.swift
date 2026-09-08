4ca50e5a20803ba2ca318bbcfa8d02e32156c983
Austin Wang <austinwang115@gmail.com>
2026-09-08 11:23:23 -0700

    fix: provide bulk toggle symbol tint

--- Sources/SidebarWorkspaceGroupBulkToggle.swift
+++ Sources/SidebarWorkspaceGroupBulkToggle.swift
@@ -28,7 +28,8 @@ struct SidebarWorkspaceGroupBulkToggle: View {
             CmuxSystemSymbolImage(
                 systemName: systemName,
                 pointSize: HeaderChromeControlMetrics.iconSize,
-                weight: .regular
+                weight: .regular,
+                tint: .secondary
             )
             .foregroundStyle(.secondary)
             .frame(
