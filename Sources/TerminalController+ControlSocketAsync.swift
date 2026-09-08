1a11df82db3aaf518d97fbdc1a5e41ace954a27f
Austin Wang <austinwang115@gmail.com>
2026-09-08 05:13:41 -0700

    fix: keep relay authorization off the socket worker

--- Sources/TerminalController+ControlSocketAsync.swift
+++ Sources/TerminalController+ControlSocketAsync.swift
@@ -61,7 +61,7 @@ extension TerminalController {
                 request = parsed
             }
 
-            let relayAuthorization = authorizeRemoteRelayRequest(request)
+            let relayAuthorization = await authorizeRemoteRelayRequestAsync(request)
             if let errorResponse = relayAuthorization.errorResponse {
                 return errorResponse
             }
